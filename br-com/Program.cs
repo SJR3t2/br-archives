3c01dddf90f87de230348d18b790fb45e360daa3
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-03 17:27:27 -0600

    added -ThreadPriority Leave

--- br-com/Program.cs
+++ br-com/Program.cs
@@ -29,7 +29,7 @@ internal static class Program
 	private static string? resultPathExt;
 	private static bool delete = false;
 	private static int threadsCount = 1;
-	private static ThreadPriority threadPriority = ThreadPriority.Lowest;
+	private static ThreadPriority? threadPriority = ThreadPriority.Lowest;
 
 	private static IEnumerator<string> files;
 	private static long filesCount = 0;
@@ -132,7 +132,17 @@ internal static class Program
 			case "-ThreadPriority":
 				try
 				{
-					threadPriority = Enum.Parse<ThreadPriority>(args[++i]);
+					var parse = args[++i];
+					switch (parse)
+					{
+					default:
+						threadPriority = Enum.Parse<ThreadPriority>(parse);
+						break;
+
+					case "Leave":
+						threadPriority = null;
+						break;
+					}
 				}
 				catch (Exception exception)
 				{
@@ -214,7 +224,7 @@ internal static class Program
 		Console.WriteLine("  -SearchShare { None, Read }");
 		Console.WriteLine("  -Delete { false, true }");
 		Console.WriteLine("  -Threads 1 //0 use the number of processors");
-		Console.WriteLine("  -ThreadPriority { Lowest, BelowNormal, Normal, AboveNormal, Highest }");
+		Console.WriteLine("  -ThreadPriority { Lowest, BelowNormal, Normal, AboveNormal, Highest, Leave }");
 		Console.WriteLine(" Optional only one");
 		Console.WriteLine("  -ResultPathExtAddBr");
 		Console.WriteLine("  -ResultPathExtAdd .br");
@@ -258,7 +268,10 @@ internal static class Program
 			for (var i = threads.Length - 1; i >= 0; --i)
 			{
 				var thread = new Thread(threadStart);
-				thread.Priority = threadPriority;
+				if (threadPriority.HasValue)
+				{
+					thread.Priority = threadPriority.Value;
+				}
 				threads[i] = thread;
 				thread.Start();
 			}
