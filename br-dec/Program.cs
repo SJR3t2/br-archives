52c16fe6e26c2a711fe600073574aab85590837c
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-03 13:23:38 -0600

    Add project files.

---
+++ br-dec/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace Innovoft.IO.Compression;
+
+internal static class Program
+{
+	internal static int Main(string[] args)
+	{
+		return 0;
+	}
+}
