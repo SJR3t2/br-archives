23bafed9fa1eb505b820f9072315ff756f2fdc7a
SJR3t2 <SJR3t2@RetzNest.com>
2026-06-05 11:54:27 -0600

    added shared class FilesEnumerable

---
+++ br-share/FilesEnumerable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Innovoft.IO.Compression;
+
+internal sealed class FilesEnumerable : IEnumerable<string>
+{
+	private readonly string path;
+	private readonly string pattern;
+	private readonly SearchOption option;
+
+	public FilesEnumerable(string path, string pattern, SearchOption option)
+	{
+		this.path = path;
+		this.pattern = pattern;
+		this.option = option;
+	}
+
+	public IEnumerator<string> GetEnumerator()
+	{
+		var enumerable = Directory.EnumerateFiles(path, pattern, option);
+		var enumerator = enumerable.GetEnumerator();
+		return enumerator;
+	}
+
+	IEnumerator IEnumerable.GetEnumerator()
+	{
+		return GetEnumerator();
+	}
+}
